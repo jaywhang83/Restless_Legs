9f9e4f6de04da95cd3af077926d96c5fd1bb9731
Jay Whang <jaywhang83@gmail.com>
2016-04-22 15:04:14 -0700

    Add inital set up for project

---
+++ src/Restless_Leg/Models/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restless_Leg.Models
+{
+    public class Location
+    {
+        [Key]
+        public int LocationId { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public virtual ICollection<Posting> Postings { get; set; }
+    }
+}
---
+++ src/Restless_Leg/Models/Posting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restless_Leg.Models
+{
+    public class Posting
+    {
+        [Key]
+        public int PostingId { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int LocationId { get; set; }
+        public virtual Location Location { get; set; } 
+    }
+}
